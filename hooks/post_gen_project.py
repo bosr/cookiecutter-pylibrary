04e25b0117eb187db554f6d9806c59be5c522e46
Romain Bossart <romain.bossart@fastmail.com>
2017-10-14 18:59:20 +0200

    simpler, with Jenkinsfile, JUnit test reports, coverage, and Pylint

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -23,6 +23,7 @@ def replace_contents(filename, what, replacement):
     with open(filename, 'w') as fh:
         fh.write(changelog.replace(what, replacement))
 
+
 if __name__ == "__main__":
     today = datetime.date.today()
     replace_contents('CHANGELOG.rst', '<TODAY>', today.strftime("%Y-%m-%d"))
@@ -37,30 +38,15 @@ if __name__ == "__main__":
     first time, as you have selected "yes" for `test_matrix_configurator` ...
 """)
     try:
+        subprocess.check_call(['git', 'init'])
         subprocess.check_call(['tox'])
     except Exception:
-        try:
-            subprocess.check_call([sys.executable, '-mtox'])
-        except Exception:
-            subprocess.check_call([sys.executable, join('ci', 'bootstrap.py')])
+        pass
 {% endif %}
 
 {%- if cookiecutter.command_line_interface|lower == 'no' %}
     os.unlink(join('src', '{{ cookiecutter.package_name|replace('-', '_') }}', '__main__.py'))
     os.unlink(join('src', '{{ cookiecutter.package_name|replace('-', '_') }}', 'cli.py'))
-{% endif %}
-
-{%- if cookiecutter.test_matrix_configurator|lower == 'no' %}
-    os.unlink(join('ci', 'templates', 'tox.ini'))
-{% endif %}
-
-{%- if cookiecutter.appveyor|lower == 'no' %}
-    os.unlink(join('ci', 'appveyor-bootstrap.py'))
-    os.unlink(join('ci', 'appveyor-download.py'))
-    os.unlink(join('ci', 'appveyor-with-compiler.cmd'))
-    os.unlink('appveyor.yml')
-    if os.path.exists(join('ci', 'templates', 'appveyor.yml')):
-        os.unlink(join('ci', 'templates', 'appveyor.yml'))
 {% endif %}
 
 {%- if cookiecutter.travis|lower == 'no' %}
@@ -93,18 +79,6 @@ if __name__ == "__main__":
         git remote add origin git@github.com:{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}.git
         git push -u origin master
 
-{% if cookiecutter.test_matrix_configurator|lower == "yes" %}
-    To reconfigure your test/CI settings run:
-
-        tox -e bootstrap
-
-    You can also run:
-
-        ci/bootstrap.py
-{% else %}
-    The project doesn't use the test matrix configurator, but in case
-    you change your mind just edit `setup.cfg` and run `ci/bootstrap.py`.
-{% endif %}
 """)
 
     command_line_interface_bin_name = '{{ cookiecutter.command_line_interface_bin_name }}'
